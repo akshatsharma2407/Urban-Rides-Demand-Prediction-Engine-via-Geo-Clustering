4dec8362574c6400944e7b1b907e1d5b9b74be19
Akshat Sharma <akshatsharma5877@gmail.com>
2026-01-14 14:14:41 +0530

    add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting the demand of rides in NYC, by dividing NYC map into cluster, using unsupervised & supervised ML algorithms.',
+    author='Akshat Sharma',
+    license='MIT',
+)
